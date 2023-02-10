effdbcf82e8ede241ee250ba486f96f6bb4c49a0
ka1mak <dev1.00.ab@gmail.com>
2023-02-10 22:22:55 +0600

    validate form

--- src/components/WorkSpace/index.js
+++ src/components/WorkSpace/index.js
@@ -4,22 +4,42 @@ import cls from './WorkSpace.module.scss'
 
 const WorkSpace = ({ action }) => {
   const [value, setValue] = React.useState('')
+  const [isEmpty, setIsEmpty] = React.useState(false)
+  const [showAlert, setShowAlert] = React.useState(false)
 
   const addNewTodo = () => {
-    action(value)
-    setValue('')
+    if (value === '') {
+      setIsEmpty(true)
+    } else if (value.length > 25) {
+      setShowAlert(true)
+    } else {
+      action(value)
+      setValue('')
+    }
+  }
+
+  if (showAlert) {
+    setTimeout(() => setShowAlert(false), 2500)
   }
 
   return (
     <div className={cls.root}>
       <h1 className={cls.title}>Todo App</h1>
-      <div>
+      <div
+        className={`${isEmpty && cls.error} ${cls.form}`}
+      >
         <input 
           placeholder='Add new todo' 
           onKeyDown={e => e.key === 'Enter' && addNewTodo()}
-          onChange={e => setValue(e.target.value)}
+          onChange={e => {
+            setIsEmpty(false)
+            setValue(e.target.value)}
+          }
           value={value}
         />
+        <div className={`${cls.alert} ${showAlert && cls.showAlert}`}>
+          Maximum length <span>25 characters!</span>
+        </div>
         <button onClick={addNewTodo}>
           <Plus />
         </button>
